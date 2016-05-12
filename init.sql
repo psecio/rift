39e0fd6156dbb00550a7b47604e1ee8492d21924
Chris Cornutt <enygma@phpdeveloper.org>
2016-05-11 20:36:57 -0500

    Updating the SQL init with user information and table changes

--- init.sql
+++ init.sql
@@ -2,12 +2,23 @@ CREATE TABLE IF NOT EXISTS `users` (
     `username` VARCHAR(50) NOT NULL,
     `password` VARCHAR(50) NOT NULL,
     `name` VARCHAR(100) NOT NULL,
+    `remember` VARCHAR(256),
+    `forgot` VARCHAR(256),
+    `created` INT,
+    `updated` INT,
     `id` INTEGER PRIMARY KEY
 );
 
 -- Insert seed data
-INSERT INTO `users` (`name`, 'username', `password`) VALUES ('Chris Cornutt', 'ccornutt', '$2y$10$7VGaKJ/D..upm0d3BNIwo.MWz1wGmyy9mjkkMNtUfsb4p/.WSrUUi');
-INSERT INTO `users` (`name`, 'username', `password`) VALUES ('Eli White', 'eliw', '$2y$10$sZfRgLzsJeonIYrlDAOPleHA0scBBHoSyV6Ev8KWGGpogkX/vyYL.');
+INSERT INTO `users`
+(`name`, 'username', `password`, `remember`, `forgot`, `created`, `updated`)
+VALUES
+('Chris Cornutt', 'ccornutt', '$2y$10$7VGaKJ/D..upm0d3BNIwo.MWz1wGmyy9mjkkMNtUfsb4p/.WSrUUi', null, null, 1459694993, 1459694993);
+
+INSERT INTO `users`
+(`name`, 'username', `password`, `remember`, `forgot`, `created`, `updated`)
+VALUES
+('Eli White', 'eliw', '$2y$10$sZfRgLzsJeonIYrlDAOPleHA0scBBHoSyV6Ev8KWGGpogkX/vyYL.', null, null, 1459694993, 1459694993);
 
 CREATE TABLE IF NOT EXISTS `posts` (
     `title` VARCHAR(100),
