6d3ff80cb8beaa074431707ae216ddf595a51c3b
Chris Cornutt <enygma@phpdeveloper.org>
2016-11-13 15:22:54 -0600

    updating the upload template for new content

--- templates/upload/index.php
+++ templates/upload/index.php
@@ -4,6 +4,15 @@
 
 <h3>File Upload</h3>
 
+<p>
+    This is an example of how to safely handle file uploads in your application. There's a bit of error handling that comes along with it on the
+    PHP side, so be sure to check out the controller.
+</p>
+<p>
+    One thing to <b>absolutely</b> not do is to store the uploaded files some place that's web accessible. In our example below, we're using a
+    "pass-through" script where we can control things like access level and prevent execution.
+</p>
+
 {% if message is defined %}
 <div class="alert alert-danger">{{ message }}</div>
 {% endif %}
@@ -26,7 +35,7 @@
 <h4>Files</h4>
 
 {% for file in files %}
-{{ file }}<br/>
+<a href="/upload/view/{{ file }}" target="_new">{{ file }}</a><br/>
 {% endfor %}
 
 {% endif %}
